d22a68b57b34d6457538598329667cab53bed14e
GuidePool <harry@guidepool.io>
2015-07-30 13:01:19 -0700

    FINAL UPDATE

--- morsList.js
+++ morsList.js
@@ -1,54 +1,68 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-  trackPageview = function(){
+    // counter starts at 0
+    Session.setDefault('counter', 0);
+    trackPageview = function() {
 
-      if(Analytics.findOne(theId)){
-          console.log('has analtyics object')
-          console.log('theId',theId)
-          var theNum = 0
-          var ThePost = Analytics.findOne(theId)
-          var totes = parseInt(Analytics.findOne(theId).pageviews)
-          console.log(totes)
-          Analytics.update({'_id':theId},{$set:{'pageviews':parseInt((totes+1))}})
-      }else{
-          var ThePost = Analytics.findOne(theId)
-          Analytics.insert({'_id':theId,'pageviews':parseInt(1)})
-          console.log('yo')
-      }
-  }
+        if (Analytics.findOne(theId)) {
+            console.log('has analtyics object')
+            console.log('theId', theId)
+            var theNum = 0
+            var ThePost = Analytics.findOne(theId)
+            var totes = parseInt(Analytics.findOne(theId).pageviews)
+            console.log(totes)
+            Analytics.update({
+                '_id': theId
+            }, {
+                $set: {
+                    'pageviews': parseInt((totes + 1))
+                }
+            })
+        } else {
+            var ThePost = Analytics.findOne(theId)
+            Analytics.insert({
+                '_id': theId,
+                'pageviews': parseInt(1)
+            })
+            console.log('yo')
+        }
+    }
 
 }
 
 if (Meteor.isServer) {
-// JsonRoutes.add("get", "/lists/:id", function (req, res, next) {
-//   var id = req.params.id;
-//
-//   JsonRoutes.sendResult(res, 200, Posts.findOne(id));
-// });
-// Enable cross origin requests for all endpoints
-JsonRoutes.setResponseHeaders({
-  "Cache-Control": "no-store",
-  "Pragma": "no-cache",
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
-});
+    // JsonRoutes.add("get", "/lists/:id", function (req, res, next) {
+    //   var id = req.params.id;
+    //
+    //   JsonRoutes.sendResult(res, 200, Posts.findOne(id));
+    // });
+    // Enable cross origin requests for all endpoints
+    JsonRoutes.setResponseHeaders({
+        "Cache-Control": "no-store",
+        "Pragma": "no-cache",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
+    });
 
-JsonRoutes.add("get", "/api/lists", function (req, res, next) {
-  // var id = req.params.id;
+    JsonRoutes.add("get", "/api/lists", function(req, res, next) {
+        // var id = req.params.id;
 
-  JsonRoutes.sendResult(res, 200, ListPosts.find().fetch());
-});
+        JsonRoutes.sendResult(res, 200, ListPosts.find().fetch());
+    });
 
-JsonRoutes.add("get", "/api/slides", function (req, res, next) {
-  // var id = req.params.id;
+    JsonRoutes.add("get", "/api/slides", function(req, res, next) {
+        // var id = req.params.id;
 
-  JsonRoutes.sendResult(res, 200, Slides.find().fetch());
-});
+        JsonRoutes.sendResult(res, 200, Slides.find().fetch());
+    });
+    JsonRoutes.add("get", "/api/analytics", function(req, res, next) {
+        // var id = req.params.id;
 
-  Meteor.startup(function () {
+        JsonRoutes.sendResult(res, 200, Analytics.find().fetch());
+    });
 
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+
+        // code to run on server at startup
+    });
 }
