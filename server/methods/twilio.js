93d81c0dfa3b109523c1190a14b079cb641286a2
Harry Ward <harryward@townsquaremedia.com>
2015-09-25 21:08:13 -0700

    token

--- server/methods/twilio.js
+++ server/methods/twilio.js
@@ -2,8 +2,8 @@ Meteor.methods({
   "sendText": function (argument) {
       client = new Twilio({
             from: '+14062008822', //14062008822 //14062008822
-            sid: 'AC59d78afa066210bf6068afcc5c5f62c0',
-            token: 'ab2a3f921e0d193c800e4b8ea8a5588a'
+            sid: 'XXXXXXXX',
+            token: 'XXXXXXXX'
         });
 client.sendSMS({
   to: '+14063708271',
